31fda56f9f9b159d55a8d8f7af1a3890bf15ae5d
Pablo Aguirre <paguirrerubio@gmail.com>
2018-06-20 16:57:49 +0200

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cloud_storage_client",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "Pablo Aguirre",
     author_email = "paguirrerubio@gmail.com",
     license = "MIT",
