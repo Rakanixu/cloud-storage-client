49e4c7e1010db874004196e3ba79a762c078bc44
Jesus Oliva <jesus@epiclabs.io>
2019-07-29 14:15:08 +0200

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cloud_storage_client",
-    version = "1.0.1",
+    version = "1.0.2",
     author = "Pablo Aguirre",
     author_email = "paguirrerubio@gmail.com",
     license = "MIT",
